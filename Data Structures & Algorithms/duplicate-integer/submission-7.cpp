58892078d8e754661f21a5aa72ac51772de4d251
Nitin Chaturvedi <120269885+Sigmanumeric78@users.noreply.github.com>
2026-08-19 23:49:36 +0530

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> hashset;
+
+        for (int n : nums) {
+            if (hashset.find(n) != hashset.end()) {
+                return true;
+            }
+
+            hashset.insert(n);
+        }
+
+        return false;
+    }
+};
